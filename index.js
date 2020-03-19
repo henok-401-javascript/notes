6906e9c91b624bbebfb306d0d5cbaa45d9170254
henok Gebremedhn <henoklwi@gmail.com>
2020-03-18 22:31:19 -0700

    finished lab 01

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
   Sends properly parsed input to the Notes library for display
 */
 console.log('hello');
+
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
+
+
+let parsedInput = new Input(process.argv.slice(2));
+let parsedNotes = new Notes(parsedInput)
